e54b14f3291a002133ed9810baeeb9c8719308c8
paulonova <paulonova144@gmail.com>
2018-09-21 15:57:39 +0200

    first commit

---
+++ src/components/DashboardPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DashboardPage = () =>(
+    <div>
+        <p>This is from my dashboard component</p>
+    </div>
+);
+
+export default DashboardPage;
---
+++ src/components/HelpPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HelpPage = () =>(
+    <div>
+        <p>This is from my help expense component</p>
+    </div>
+);
+
+
+
+export default HelpPage;
